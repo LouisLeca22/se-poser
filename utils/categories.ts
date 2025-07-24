901536f181ab86653d234b35300b7da096ad8564
Weboscopy <weboscopy@gmail.com>
2025-07-24 09:23:08 +0200

    fix categories label

--- utils/categories.ts
+++ utils/categories.ts
@@ -65,7 +65,7 @@ export const categories: Category[] = [
     },
     {
         name: 'magic',
-        label: "féerique",
+        label: "Féerique",
         icon: GiMushroomHouse,
     },
     {
